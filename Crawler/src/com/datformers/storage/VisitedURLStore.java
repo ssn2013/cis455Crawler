2e4ba3a19eab8abfee541397fe0d7c34f537c693
dashdeep13 <dashdeep13@gmail.com>
2015-05-01 04:02:26 -0400

    visited store

---
+++ Crawler/src/com/datformers/storage/VisitedURLStore.java
@@ -0,0 +1,34 @@
+package com.datformers.storage;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.sleepycat.persist.model.Entity;
+import com.sleepycat.persist.model.PrimaryKey;
+
+/*
+ * Entity class of a fetched URL's contents
+ */
+@Entity
+public class VisitedURLStore {
+	@PrimaryKey
+	BigInteger url;
+	
+	
+	public BigInteger getUrl() {
+		return url;
+	}
+
+
+	public void setUrl(BigInteger url) {
+		this.url = url;
+	}
+
+
+	//TODO: add hashed Doc Id (BigInteger)
+	//TODO: list of extracted URLs
+	public VisitedURLStore() {
+		
+	}
+	}
